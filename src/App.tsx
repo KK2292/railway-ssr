d9ed27c48e9ace0cc87a3d101c7a79b4c93f8f80
KK2292 <kohei.kwn@gmail.com>
2024-09-26 13:57:35 +0900

    Refactor TypeScript and Webpack configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { css, Global } from "@emotion/react"
-import { Container, Snackbar, Stack, Typography } from "@mui/material"
-import { useState } from "react";
+import { css, Global } from "@emotion/react";
+import { Container, Stack, Typography } from "@mui/material";
 
 export const App=()=> {
   const myData = ["名前:河野公平", "年齢:22", "趣味:Splatoon",]
 
-  const [open, setOpen] = useState(true);
-  
-  const handleClose = () => {
-    setOpen(false);
-  }
   return (
     <>
       <Global
@@ -21,7 +15,6 @@ export const App=()=> {
           }
         `}
       />
-      <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} autoHideDuration={5000} open={open} onClose={handleClose} message="Hello World"/>
       <Stack spacing={4} sx={{display:"flex",flexDirection:"column",alignItems:"center"}}>
       <Typography sx={{fontSize:"64px",color:"#fff"}}>My Profile</Typography>
         <Container sx={{backgroundColor:"#111",maxWidth:"500px",borderRadius:"10px",padding:"32px"}}>

c27a7d8e377d3607f1a3504ae85ef48de479e33e
KK2292 <kohei.kwn@gmail.com>
2024-09-26 09:25:33 +0900

    Add Vite configuration file

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { css, Global } from "@emotion/react"
+import { Container, Stack, Typography } from "@mui/material"
+
+export const App=()=> {
+ const myData=["名前:河野公平","年齢:22","趣味:Splatoon",]
+  return (
+    <>
+      <Global
+        styles={css`
+          body {
+            background-color: #666;
+            margin: 0;
+            padding: 80px;
+          }
+        `}
+      />
+      <Stack spacing={4} sx={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <Typography sx={{fontSize:"64px",color:"#fff"}}>My Profile</Typography>
+        <Container sx={{backgroundColor:"#111",maxWidth:"500px",borderRadius:"10px",padding:"32px"}}>
+          <Stack spacing={2}>
+        {myData.map((data,index)=>(
+          <Typography key={index} sx={{color:"#fff"}}>{data}</Typography>
+        ))}</Stack>
+        </Container>
+      </Stack>
+    </>
+  )
+}
+

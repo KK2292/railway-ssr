0d8738823eb8df8bb89ad300ebe00e56b6cb9e4c
KK2292 <kohei.kwn@gmail.com>
2024-09-28 16:48:01 +0900

    Refactor App component to use TypeScript and update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { css, Global } from "@emotion/react";
 import { Container, Stack, Typography } from "@mui/material";
 
-export const App=()=> {
-  const myData = ["名前:河野公平", "年齢:22", "趣味:Splatoon",]
+export const App = () => {
+  const myData = ["名前:河野公平", "年齢:22", "趣味:Splatoon"];
 
   return (
     <>
@@ -15,16 +15,30 @@ export const App=()=> {
           }
         `}
       />
-      <Stack spacing={4} sx={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-      <Typography sx={{fontSize:"64px",color:"#fff"}}>My Profile</Typography>
-        <Container sx={{backgroundColor:"#111",maxWidth:"500px",borderRadius:"10px",padding:"32px"}}>
+      <Stack
+        spacing={4}
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography sx={{ fontSize: "64px", color: "#fff" }}>
+          My Profile
+        </Typography>
+        <Container
+          sx={{
+            backgroundColor: "#111",
+            maxWidth: "500px",
+            borderRadius: "10px",
+            padding: "32px",
+          }}
+        >
           <Stack spacing={2}>
-        {myData.map((data,index)=>(
-          <Typography key={index} sx={{color:"#fff"}}>{data}</Typography>
-        ))}</Stack>
+            {myData.map((data, index) => (
+              <Typography key={index} sx={{ color: "#fff" }}>
+                {data}
+              </Typography>
+            ))}
+          </Stack>
         </Container>
       </Stack>
     </>
-  )
-}
-
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import {App} from "./App";
+import { App } from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -8,5 +8,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Router basename="/railway-ssr">
       <App />
     </Router>
-  </StrictMode>,
+  </StrictMode>
 );
